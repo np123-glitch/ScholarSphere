e709d291d0e15c17f7445ad1d516689b018278a4
Neel Prasad <65573503+np123-glitch@users.noreply.github.com>
2025-02-21 17:13:50 -0600

    Updated main.py to ignore pfp folder

--- ScholarSphereServer/main.py
+++ ScholarSphereServer/main.py
@@ -300,6 +300,8 @@ def list_user_files(current_user, username):
 
     file_list = []
     for fname in os.listdir(user_upload_dir):
+        if fname == "pfp":
+            continue
         file_url = f"http://scholarsphere.anythingnew.today/files/{username}/{fname}"
         file_list.append({
             "fileName": fname,
