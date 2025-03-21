1ce92bda668d79e3a29bf55982827ac61a851fcc
Neel Prasad <65573503+np123-glitch@users.noreply.github.com>
2025-03-20 19:01:12 -0500

    Fixed server address

--- components/Config.tsx
+++ components/Config.tsx
@@ -2,7 +2,7 @@
 
 const CONFIG = {
     // Replace '192.168.1.100' with your actual IP address
-    API_BASE_URL: 'http://172.20.10.8:5000',
+    API_BASE_URL: 'http://scholarsphere.anythingnew.today',
     // You can add more configurations here
     // e.g., TIMEOUT: 5000,
   };
