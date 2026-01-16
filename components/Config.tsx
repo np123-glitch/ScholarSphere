4b482e747c982ee77b285727dafc494aa24f612a
Neel Prasad <65573503+np123-glitch@users.noreply.github.com>
2026-01-15 22:12:21 -0600

    update api url

--- components/Config.tsx
+++ components/Config.tsx
@@ -2,7 +2,7 @@
 
 const CONFIG = {
     // Replace '192.168.1.100' with your actual IP address
-    API_BASE_URL: 'http://scholarsphere.anythingnew.today',
+    API_BASE_URL: 'http://scholarsphere.scholarping.com',
     // You can add more configurations here
     // e.g., TIMEOUT: 5000,
   };
